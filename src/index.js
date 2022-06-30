05e55e6704b72986ea825318be2e3c9fbe2be3bd
petrissakov <petrissakov@gmail.com>
2022-06-30 22:56:52 +0600

    feat: add reverse function to perform task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    n_str = String(Math.abs(n));
+    result = []
+    for (let i=n_str.length; i>=0; i--){
+
+        result.push(n_str[i]);
+    }
+    return Number(result.join(''));
+
 }
+
+//  console.log(module.exports(-123));
